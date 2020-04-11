e600eb6721735ce47ed91c50fbb09a97e452f0b1
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-04-10 22:43:36 -0400

    main.js complete

--- public/main.js
+++ public/main.js
@@ -4,3 +4,71 @@ const video = document.querySelector('video')
 let client = {}
 
 // get video stream
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+})
+    .then(stream => {
+        socket.emit('NewClient')
+        video.srcObject = stream
+        video.play()
+
+        //used to initialize a peer
+        function InitPeer(type) {
+            let peer = new Peer({ initiator: (type == 'init') ? true : false, stream: stream, trickle: false })
+            peer.on('stream', function(stream){
+                CreateVideo(stream)
+            })
+            peer.on('close', function(){
+                document.getElementById("peerVideo").remove();
+                peer.destroy()
+            })
+            return peer
+        }
+
+        //for peer of type init
+        function MakePeer(){
+            client.gotAnswer = false
+            let peer = InitPeer('init')
+            peer.on('signal', function(data){
+                if(!client.gotAnswer){
+                    socket.emit('Offer', data)
+                }
+            })
+            client.peer = peer
+        }
+
+        //for peer of type not init
+        function FrontAnswer(offer){
+            let peer = InitPeer('notInit')
+            peer.on('signal', (data) => {
+                socket.emit('Answer', data)
+            })
+            peer.signal(offer)
+        }
+
+        function SignalAnswer(answer){
+            client.gotAnswer = true
+            let peer = client.peer
+            peer.signal(answer)
+        }
+
+        function CreateVideo(stream){
+            let video = document.createElement('video')
+            video.id = 'peerVideo'
+            video.srcObject = stream
+            video.class = 'embed-responsive-item'
+            document.querySelector('#peerDiv').appendChild(video)
+        }
+
+        function SessionActive(){
+            document.write('Session active. Please come back later!')
+        }
+
+        socket.on('BackOffer', FrontAnswer)
+        socket.on('BackAnswer', SignalAnswer)
+        socket.on('SessionActive', SessionActive)
+        socket.on('CreatePeer', MakePeer)
+
+    })
+    .catch(err => document.write(err))
